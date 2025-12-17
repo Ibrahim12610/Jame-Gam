0de0c8f12a1beb511d28534d269fd8ddf38b3757
Waffles <zacharye0427@gmail.com>
2025-12-17 17:48:13 -0600

    Player Movement YAY

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    //Components
+    Rigidbody2D rb;
+
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Vector2 moveVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            moveVector += Vector2.up;
+        if (Input.GetKey(KeyCode.A))
+            moveVector += Vector2.left;
+        if (Input.GetKey(KeyCode.S))
+            moveVector += Vector2.down;
+        if (Input.GetKey(KeyCode.D))
+            moveVector += Vector2.right;
+
+        moveVector.Normalize();
+        moveVector *= moveSpeed;
+
+        rb.linearVelocity = moveVector;
+    }
+}
