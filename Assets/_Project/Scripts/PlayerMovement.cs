98b3d8a180c5b9c9a6a071020f1d6dc4ca5f960c
Junior <94208651+Junior4144@users.noreply.github.com>
2025-12-17 21:45:20 -0600

    adds PlayerAnimator script and fixes animation warning
    Walk back animation was not correctly configure in the player animator. adding it to animator fixed the issue.

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -3,31 +3,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    //Components
-
     Rigidbody2D rb;
-    [Header("Animations")]
-    private Animator animator;
-    private SpriteRenderer spriteRenderer;
-    private const string State_idle_forward= "forward_idle";
-    private const string State_idle_side= "side_idle";
-    private const string State_idle_back= "back_idle";
-    private const string State_walk_forward= "forward_walk";
-    private const string State_walk_side= "side_walk";
-    private const string State_walk_back = "walk_back";
-
+    
     [Header("Settings")]
     [SerializeField] float moveSpeed;
-
     
     private Vector2 lastMoveDirection = Vector2.down;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+    
     void Update()
     {
         Vector2 moveVector = Vector2.zero;
@@ -45,70 +32,5 @@ public class PlayerMovement : MonoBehaviour
         moveVector *= moveSpeed;
 
         rb.linearVelocity = moveVector;
-
-        
-        UpdateAnimations(moveVector);
-    }
-
-    void UpdateAnimations(Vector2 moveVector)
-    {
-        
-        if (moveVector.magnitude > 0.1f)
-        {
-            lastMoveDirection = moveVector.normalized;
-        }
-
-        
-        bool isMoving = moveVector.magnitude > 0.1f;
-        Vector2 currentDirection = isMoving ? moveVector.normalized : lastMoveDirection;
-
-       
-        if (spriteRenderer != null)
-        {
-            float absX = Mathf.Abs(currentDirection.x);
-            float absY = Mathf.Abs(currentDirection.y);
-            
-           
-            if (absX > absY)
-            {
-                
-                spriteRenderer.flipX = currentDirection.x < 0;
-            }
-        }
-
-        string animationState = GetAnimationState(isMoving, currentDirection);
-
-        animator.Play(animationState);
-    }
-
-    string GetAnimationState(bool isMoving, Vector2 direction)
-    {
-       
-        float absX = Mathf.Abs(direction.x);
-        float absY = Mathf.Abs(direction.y);
-
-        string stateName;
-
-        if (absY > absX)
-        {
-            
-            if (direction.y < 0)
-            {
-               
-                stateName = isMoving ? State_walk_forward : State_idle_forward;
-            }
-            else
-            {
-                
-                stateName = isMoving ? State_walk_back : State_idle_back;
-            }
-        }
-        else
-        {
-            
-            stateName = isMoving ? State_walk_side : State_idle_side;
-        }
-
-        return stateName;
     }
 }
