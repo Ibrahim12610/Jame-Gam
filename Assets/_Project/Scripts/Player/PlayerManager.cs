0566998b14f11e7302b49ce9c4d31f027ff1f00a
Junior <94208651+Junior4144@users.noreply.github.com>
2025-12-19 15:12:10 -0600

    fixes player manager

--- Assets/_Project/Scripts/Player/PlayerManager.cs
+++ Assets/_Project/Scripts/Player/PlayerManager.cs
@@ -7,6 +7,12 @@ public class PlayerManager : MonoBehaviour
 
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
     }
 
