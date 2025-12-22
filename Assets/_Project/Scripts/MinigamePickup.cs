74cd48bb384f72fc6d9fad7df5a7f36d8bbb7e88
Junior <94208651+Junior4144@users.noreply.github.com>
2025-12-22 13:47:22 -0600

    adds audio to minigames, background
    Sound will be changed, mostly a system implementation

--- Assets/_Project/Scripts/MinigamePickup.cs
+++ Assets/_Project/Scripts/MinigamePickup.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 public class MinigamePickup : MonoBehaviour
 {
     [SerializeField] private int amount;
+    [SerializeField] private GameObject soundSpawnerPrefab;
+    [SerializeField] private AudioClip startMiniGameClip;
+    [SerializeField] private AudioClip minigameSuccessClip;
+    [SerializeField] private AudioClip minigameFailClip;
     
     public GameObject minigameCanvas;
     private bool _playerInside = false;
     private MiniGame _currentGame;
     
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && _playerInside && _currentGame == null)
         {
+            HandeAudio(startMiniGameClip);
             HandleMiniGameStart();
         }
     }
@@ -52,6 +56,14 @@ public class MinigamePickup : MonoBehaviour
         _currentGame.OnMiniGameFail += HandleMiniGameFail;
     }
     
+    private void HandeAudio(AudioClip audioClip)
+    {
+        var go = Instantiate(soundSpawnerPrefab);
+        var soundSpawner =  go.GetComponent<SoundSpawnerController>();
+        soundSpawner.clip = audioClip;
+        soundSpawner.PlayClip();
+    }
+    
     private void CleanupSubscriptions()
     {
         if (_currentGame == null) return;
@@ -65,6 +77,7 @@ public class MinigamePickup : MonoBehaviour
     private void HandleMiniGameSuccess()
     {
         CleanupSubscriptions();
+        HandeAudio(minigameSuccessClip);
         GameUtility.PauseMenuEnableLogic();
         MiniGameManager.Instance.OnMiniGameComplete(1);
         Destroy(gameObject);
@@ -72,6 +85,7 @@ public class MinigamePickup : MonoBehaviour
     
     private void HandleMiniGameFail()
     {
+        HandeAudio(minigameFailClip);
         CleanupSubscriptions();
         GameUtility.PauseMenuEnableLogic();
     }
