6c1527abbee34e9f961bdcb85ac73aa102189b3f
Junior <94208651+Junior4144@users.noreply.github.com>
2025-12-22 20:08:03 -0600

    polishing
    adds indicator images and adds feature to map

--- Assets/_Project/Scripts/Units/Santa/SantaAttackController.cs
+++ Assets/_Project/Scripts/Units/Santa/SantaAttackController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,10 +8,16 @@ public class SantaAttackController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         if (!otherCollider.CompareTag("Player")) return;
+
+        StartCoroutine(HandleAttackSequence());
+    }
+
+    private IEnumerator HandleAttackSequence()
+    {
+        yield return new WaitForSeconds(0.5f);
         
-        PlayerManager.Instance.DisablePlayerLogic("Death");
+        PlayerManager.Instance.DisablePlayerLogic("PlayerDeath");
         PlayerManager.Instance.HandleonKillSequence();
         SceneChangeManager.Instance.RestartScene(SceneManager.GetActiveScene().name);
     }
-    
 }
