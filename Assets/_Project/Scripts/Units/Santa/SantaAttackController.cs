a0fa690bdb78df4efc91e200059e4b0001efc7f3
Joseph Giap <145407086+JosephGiap123@users.noreply.github.com>
2025-12-24 01:37:37 -0600

    fixed death anim and punch anim

--- Assets/_Project/Scripts/Units/Santa/SantaAttackController.cs
+++ Assets/_Project/Scripts/Units/Santa/SantaAttackController.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class SantaAttackController : MonoBehaviour
 {
     [SerializeField] private AudioClip deathSound;
+    [SerializeField] private float deathTimer = 2f;
     private AudioSource _audioSource;
     private SantaAI _santaAI;
     private bool _hasAttackSequenceStarted = false;
@@ -13,7 +14,7 @@ public class SantaAttackController : MonoBehaviour
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
-        _santaAI =  GetComponentInParent<SantaAI>();
+        _santaAI = GetComponentInParent<SantaAI>();
     }
 
     private void OnTriggerEnter2D(Collider2D otherCollider)
@@ -22,25 +23,25 @@ public class SantaAttackController : MonoBehaviour
 
         if (_hasAttackSequenceStarted) return;
         _hasAttackSequenceStarted = true;
-        
+
         StartCoroutine(HandleAttackSequence());
     }
 
     private IEnumerator HandleAttackSequence()
     {
         yield return new WaitForSeconds(0.5f);
-        
+
         PlayerManager.Instance.DisablePlayerLogic("PlayerDeath");
         PlayerManager.Instance.GetPlayerAnimator().TriggerDeath();
         PlayerManager.Instance.HandleDeathColliderLogic();
         _audioSource.PlayOneShot(deathSound);
         _santaAI.hasKilledPlayer = true;
-        
-        yield return new WaitForSeconds(5f);
-        
+
+        yield return new WaitForSeconds(deathTimer);
+
         //PlayerManager.Instance.HandleonKillSequence();
-        
-        
+
+
         SceneChangeManager.Instance.RestartScene(SceneManager.GetActiveScene().name);
     }
 }
